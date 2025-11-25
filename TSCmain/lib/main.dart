75785c9e1e80159bb52238ec9bb68f21271e63da
Vishruth31 <vishruthgirish31@gmail.com>
2025-11-25 22:27:43 +0530

    Auto-sync: file changes

--- TSCmain/lib/main.dart
+++ TSCmain/lib/main.dart
@@ -1,12 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:flutter_dotenv/flutter_dotenv.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
+import 'splash_screen.dart';   // 👈 Your splash file in lib/
 
-  // Load the .env file
-  await dotenv.load(fileName: ".env");
 
+void main() {
   runApp(const MyApp());
 }
 
@@ -16,57 +13,15 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
-  }
-}
+      debugShowCheckedModeBanner: false,
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
+      // Start app with splash screen
+      home: const SplashScreenPages(),
 
-  final String title;
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      _counter++;
-    });
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text(widget.title),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            const Text('You have pushed the button this many times:'),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
+      // All navigation routes
+      routes: {
+        '/home': (context) => const HomePage(),
+      },
     );
   }
 }
