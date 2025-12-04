46b158f3d93efc409dc09973d62f99cbdc5ce901
Vishruth31 <vishruthgirish31@gmail.com>
2025-12-04 20:54:33 +0530

    Auto-sync: file changes

--- TSCmain/lib/widgets/bottom_nav/home_bottom_nav_bar.dart
+++ TSCmain/lib/widgets/bottom_nav/home_bottom_nav_bar.dart
@@ -34,17 +34,17 @@ class HomeBottomNavBar extends StatelessWidget {
       ),
       child: Row(
         children: [
-          _navItem(Icons.home, "Home", 0),
-          _navItem(Icons.favorite_border, "Favourite", 1),
-          _navItem(Icons.grid_view_rounded, "All", 2),
-          _navItem(Icons.shopping_bag_outlined, "Cart", 3),
-          _navItem(Icons.person, "Profile", 4),
+          _navItem(Icons.home, 0),
+          _navItem(Icons.favorite_border, 1),
+          _navItem(Icons.grid_view_rounded, 2),
+          _navItem(Icons.shopping_bag_outlined, 3),
+          _navItem(Icons.person, 4),
         ],
       ),
     );
   }
 
-  Widget _navItem(IconData icon, String label, int index) {
+  Widget _navItem(IconData icon, int index) {
     final isSelected = selectedIndex == index;
 
     return Expanded(
@@ -55,18 +55,8 @@ class HomeBottomNavBar extends StatelessWidget {
           children: [
             Icon(
               icon,
-              size: 24,
+              size: 30, // increased icon size
               color: isSelected ? Colors.pinkAccent : Colors.grey.shade500,
-            ),
-            const SizedBox(height: 4),
-            Text(
-              label,
-              style: TextStyle(
-                fontSize: 11,
-                fontWeight: FontWeight.w600,
-                color:
-                    isSelected ? Colors.pinkAccent : Colors.grey.shade500,
-              ),
             ),
           ],
         ),
