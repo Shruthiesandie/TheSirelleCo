8e085ee2512f4deb2124ecc647989775185b564d
Shruthiesandie <nagashruthiyelika@gmail.com>
2025-12-08 14:44:20 +0530

    Update splash_screen.dart

--- TSCmain/lib/splash/splash_screen.dart
+++ TSCmain/lib/splash/splash_screen.dart
@@ -51,8 +51,8 @@ class _SplashScreenState extends State<SplashScreen>
             opacity: _fade,
             child: Image.asset(
               "assets/splash/splash.png",
-              width: 750,
-              height: 750,
+              width: 700,
+              height: 700,
               fit: BoxFit.contain,
             ),
           ),
