e821e6297a9465778e376b252cd73e2d6a4aa57d
Vishruth31 <vishruthgirish31@gmail.com>
2025-12-14 19:05:06 +0530

    Auto-sync: file changes

--- TSCmain/lib/data/seed_products.dart
+++ TSCmain/lib/data/seed_products.dart
@@ -1,35 +1,24 @@
-class Product {
-  final String id;
-  final String name;
-  final String category;
-  final int price;
-  final String thumbnail;
+import '../models/product.dart';
 
-  Product({
-    required this.id,
-    required this.name,
-    required this.category,
-    required this.price,
-    required this.thumbnail,
-  });
-
-  factory Product.fromMap(Map<String, dynamic> map) {
-    return Product(
-      id: map['id'],
-      name: map['name'],
-      category: map['category'],
-      price: map['price'],
-      thumbnail: map['thumbnail'],
-    );
-  }
-
-  Map<String, dynamic> toMap() {
-    return {
-      'id': id,
-      'name': name,
-      'category': category,
-      'price': price,
-      'thumbnail': thumbnail,
-    };
-  }
-}
+final List<Product> seedProducts = [
+  Product(
+    id: 'b1',
+    name: 'Bottle 1',
+    category: 'Bottles',
+    price: 899,
+    thumbnail: 'assets/images/bottles/b1/bottle.png',
+    images: [
+      'assets/images/bottles/b1/bottle.png',
+    ],
+  ),
+  Product(
+    id: 'b2',
+    name: 'Bottle 2',
+    category: 'Bottles',
+    price: 899,
+    thumbnail: 'assets/images/bottles/b2/bottle2.png',
+    images: [
+      'assets/images/bottles/b2/bottle2.png',
+    ],
+  ),
+];
