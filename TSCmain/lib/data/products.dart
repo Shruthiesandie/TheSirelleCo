6b886de0df8c24e8064f144fd63dafba9c6dd90f
Vishruth31 <vishruthgirish31@gmail.com>
2025-12-17 21:38:07 +0530

    Auto-sync: file changes

--- TSCmain/lib/data/products.dart
+++ TSCmain/lib/data/products.dart
@@ -4,7 +4,7 @@ final List<Product> products = [
   Product(
     id: "b1",
     name: "candle 1",
-    thumbnail: "assets/images/bottles/b1/bottle.png",
+    thumbnail: "assets/images/bottles/b1/12345.png",
     images: [
       "assets/images/candle/c1/glass1.png",
       "assets/images/candle/c1/cdiff1/glass11.png",
@@ -16,7 +16,7 @@ final List<Product> products = [
   Product(
     id: "b2",
     name: "Bottle 2",
-    thumbnail: "assets/splash/12345.png",
+    thumbnail: "assets/splash/456.png",
     images: [
       "assets/images/bottles/b2/bottle.png",
       "assets/splash/123.png",
