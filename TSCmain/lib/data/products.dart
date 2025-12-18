8262c9b765509875eb432d10e8502ffa10598143
Vishruth31 <vishruthgirish31@gmail.com>
2025-12-18 13:00:37 +0530

    Auto-sync: file changes

--- TSCmain/lib/data/products.dart
+++ TSCmain/lib/data/products.dart
@@ -25,4 +25,69 @@ final List<Product> products = [
   ),
 
   // 👉 repeat till b8
+Product(
+  id: "b3",
+  name: "Bottle 3",
+  thumbnail: "assets/images/bottles/b3/bottle3.png",
+  images: [
+    "assets/images/bottle/b3/bdiff/bottle31.png",
+    "assets/images/bottle/b3/bdiff/bottle32.png",
+    "assets/images/bottle/b3/bdiff/bottle33.png",
+  ],
+  ),
+
+  Product(
+  id: "b4",
+  name: "Bottle 4",
+  thumbnail: "assets/images/bottles/b4/bottle4.png",
+  images: [
+    "assets/images/bottle/b4/bdiff/bottle41.png",
+    "assets/images/bottle/b4/bdiff/bottle42.png",
+    "assets/images/bottle/b4/bdiff/bottle43.png",
+  ],
+  ),
+
+  Product(
+  id: "b5",
+  name: "Bottle 5",
+  thumbnail: "assets/images/bottles/b5/bottle5.png",
+  images: [
+    "assets/images/bottle/b5/bdiff/bottle51.png",
+    "assets/images/bottle/b5/bdiff/bottle52.png",
+    "assets/images/bottle/b5/bdiff/bottle53.png",
+  ],
+  ),
+
+  Product(
+  id: "b6",
+  name: "Bottle 6",
+  thumbnail: "assets/images/bottles/b6/bottle6.png",
+  images: [
+    "assets/images/bottle/b6/bdiff/bottle61.png",
+    "assets/images/bottle/b6/bdiff/bottle62.png",
+    "assets/images/bottle/b6/bdiff/bottle63.png",
+  ],
+  ),
+
+  Product(
+  id: "b7",
+  name: "Bottle 7",
+  thumbnail: "assets/images/bottles/b7/bottle7.png",
+  images: [
+    "assets/images/bottle/b7/bdiff/bottle71.png",
+    "assets/images/bottle/b7/bdiff/bottle72.png",
+    "assets/images/bottle/b7/bdiff/bottle73.png",
+  ],
+  ),
+
+  Product(
+  id: "b8",
+  name: "Bottle 8",
+  thumbnail: "assets/images/bottles/b8/bottle8.png",
+  images: [
+    "assets/images/bottle/b8/bdiff/bottle81.png",
+    "assets/images/bottle/b8/bdiff/bottle82.png",
+    "assets/images/bottle/b8/bdiff/bottle83.png",
+  ],
+  ),
 ];
