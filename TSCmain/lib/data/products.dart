42dc4e435d0430e87a2b5b6c21ad1d6d91fce1a2
Vishruth31 <vishruthgirish31@gmail.com>
2025-12-18 13:25:40 +0530

    Auto-sync: file changes

--- TSCmain/lib/data/products.dart
+++ TSCmain/lib/data/products.dart
@@ -91,38 +91,77 @@ Product(
   ],
   ),
 
+  // ================= CANDLES =================
+
   Product(
-  id: "c1",
-  name: "Candle 1",
-  thumbnail: "assets/images/candle/c1/glass1.png",
-  images: [
-    "assets/images/candle/c1/candle.png",
-    "assets/images/candle/c1/cdiff/glass11.png",
-    "assets/images/candle/c1/cdiff/glass12.png",
-    "assets/images/candle/c1/cdiff/glass13.png",
-  ],
+    id: "c1",
+    name: "Candle 1",
+    thumbnail: "assets/images/candle/c1/glass1.png",
+    images: [
+      "assets/images/candle/c1/glass1.png",
+      "assets/images/candle/c1/cdiff1/glass11.png",
+      "assets/images/candle/c1/cdiff1/glass12.png",
+      "assets/images/candle/c1/cdiff1/glass13.png",
+    ],
   ),
+
   Product(
     id: "c2",
     name: "Candle 2",
     thumbnail: "assets/images/candle/c2/glass2.png",
     images: [
-      "assets/images/candle/c2/candle2.png",
+      "assets/images/candle/c2/glass2.png",
       "assets/images/candle/c2/cdiff2/glass21.png",
       "assets/images/candle/c2/cdiff2/glass22.png",
       "assets/images/candle/c2/cdiff2/glass23.png",
-  ],
+    ],
   ),
+
   Product(
     id: "c3",
     name: "Candle 3",
     thumbnail: "assets/images/candle/c3/cake1.png",
     images: [
-      "assets/images/candle/c3/candle3.png",
-      "assets/images/candle/c3/cdiff3/glass31.png",
-      "assets/images/candle/c3/cdiff3/glass32.png",
-      "assets/images/candle/c3/cdiff3/glass33.png",
+      "assets/images/candle/c3/cake1.png",
+      "assets/images/candle/c3/cdiff3/cake11.png",
+      "assets/images/candle/c3/cdiff3/cake12.png",
+      "assets/images/candle/c3/cdiff3/cake13.png",
     ],
   ),
 
+  Product(
+    id: "c4",
+    name: "Candle 4",
+    thumbnail: "assets/images/candle/c4/glass3.png",
+    images: [
+      "assets/images/candle/c4/glass3.png",
+      "assets/images/candle/c4/cdiff4/glass31.png",
+      "assets/images/candle/c4/cdiff4/glass32.png",
+      "assets/images/candle/c4/cdiff4/glass33.png",
+    ],
+  ),
+
+  Product(
+    id: "c5",
+    name: "Candle 5",
+    thumbnail: "assets/images/candle/c5/macarron.png",
+    images: [
+      "assets/images/candle/c5/macarron.png",
+      "assets/images/candle/c5/cdiff5/macarron1.png",
+      "assets/images/candle/c5/cdiff5/macarron2.png",
+      "assets/images/candle/c5/cdiff5/macarron3.png",
+    ],
+  ),
+
+  Product(
+    id: "c6",
+    name: "Candle 6",
+    thumbnail: "assets/images/candle/c6/popcorn.png",
+    images: [
+      "assets/images/candle/c6/popcorn.png",
+      "assets/images/candle/c6/cdiff6/popcorn1.png",
+      "assets/images/candle/c6/cdiff6/popcorn2.png",
+      "assets/images/candle/c6/cdiff6/popcorn3.png",
+    ],
+  ),
 ];
